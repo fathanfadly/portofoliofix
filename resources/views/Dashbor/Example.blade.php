d54f2791406862c182436aff95a635f761e3cc68
fathanfadly <113814360+fathanfadly@users.noreply.github.com>
2024-04-24 00:25:51 +0700

    Update Example.blade.php

--- resources/views/Dashbor/Example.blade.php
+++ resources/views/Dashbor/Example.blade.php
@@ -7,7 +7,8 @@
     <title>Example Page</title>
     <!-- Bootstrap CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel ="stylesheet" href="css/bootstrap.css" />
 
 </head>
